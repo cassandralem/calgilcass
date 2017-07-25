8e7ce89bae9f67b703f49315f4d98f32dfaf6a7b
Gilbertome1 <gmalfabon101@gmail.com>
2017-07-24 17:18:20 -0700

    added two videos and js

---
+++ javascript/home.js
@@ -0,0 +1,39 @@
+var tag = document.createElement('script');
+
+   tag.src = "https://www.youtube.com/iframe_api";
+   var firstScriptTag = document.getElementsByTagName('script')[0];
+   firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+   var player1;
+   var player2;
+
+   function onYouTubeIframeAPIReady() {
+       player1 = new YT.Player('player1', {
+         height: '500',
+         width: '500',
+         videoId: 'yHboxvCGwyI',
+         playerVars: { 'autoplay': 0, 'controls': 0, 'disablekb':1 },
+         events: {
+               'onReady': onPlayerReady
+           }
+       });
+
+       player2 = new YT.Player('player2', {
+         height: '500',
+         width: '500',
+         videoId: 'fcsDG_jVYbc',
+         playerVars: { 'autoplay': 0, 'controls': 0, 'disablekb':1 },
+         events: {
+               'onReady': stopVideo
+           }
+       });
+   }
+
+
+   function onPlayerReady(event) {
+       event.target.playVideo();
+   }
+
+   function stopVideo() {
+       player.stopVideo();
+   }
